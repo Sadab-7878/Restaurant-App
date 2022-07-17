11ffdad4377f30b31c747b79b3394b76e7e930d0
Sadab76 <sadabuzzaman75@gmail.com>
2022-07-17 20:04:53 +0600

    t6

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -11,7 +11,7 @@ const useState = () => {
             <span></span>
             <span></span>
             <span></span>
-            In
+            Inerr
         </div>
         <div className="button2">
             <span></span>
