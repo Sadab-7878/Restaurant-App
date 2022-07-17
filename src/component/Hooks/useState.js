795ede398e5ce5e2ec1aa9f54e73138e600bcf1a
Sadab76 <sadabuzzaman75@gmail.com>
2022-07-17 23:03:20 +0600

    werwer

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const useState = () => {
+  const initialData = 10;
+  const [myNum, setMyNum] = React.useState(initialData);
+  console.log(myNum);
   return (
     <>
-     <div className="center_div">
-        <p>10</p>
-        <div className="button2">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Increment
+      <div className="center_div">
+        <p>{myNum}</p>
+        <div className="button2" onClick={() => setMyNum(myNum + 1)}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Increment
         </div>
-        <div className="button2">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Decement
+        <div
+          className="button2"
+          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Decement
         </div>
-        
-        </div>   
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default useState;
