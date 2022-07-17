ce42c89b901c25522c60062fe943fba40f270879
Sadab76 <sadabuzzaman75@gmail.com>
2022-07-17 23:22:18 +0600

    fdsfs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Resturant from './component/Basics/Resturant';
 import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
 
 const App = () => {
-  return  <UseState />
+  return  <UseEffect />
 }
 
 export default App
