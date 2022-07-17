4b45ee250c860dce4a4c306c2e70092226b048ef
sadab76 <sadabuzzaman75@gmail.com>
2022-07-17 19:48:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Resturant from './component/Basics/Resturant';
+import UseState from './component/Hooks/useState';
+
+const App = () => {
+  return  <UseState />
+}
+
+export default App
---
+++ src/component/Hooks/useState.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./style.css"
+
+const useState = () => {
+  return (
+    <>
+     <div className="center_div">
+        <p>10</p>
+        <div className="button2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Increment
+        </div>
+        <div className="button2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Decrement
+        </div>
+        
+        </div>   
+    </>
+  )
+}
+
+export default useState;
