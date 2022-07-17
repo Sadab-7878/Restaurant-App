352bf18c10d5b0b72b1f456544c3bdedd98a8f18
Sadab76 <sadabuzzaman75@gmail.com>
2022-07-17 20:31:33 +0600

    fdsfsd

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -11,7 +11,7 @@ const useState = () => {
             <span></span>
             <span></span>
             <span></span>
-            Increment
+            Incre
         </div>
         <div className="button2">
             <span></span>
